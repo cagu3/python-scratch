e899d80ba7797406bb285dc3e57f49b11dadfe71
cagu3 <cguthrie37@gmail.com>
2019-10-11 22:08:32 -0600

    Second commit

--- bg.py
+++ bg.py
@@ -1,32 +1,37 @@
 import shutil, os, datetime
 from PIL import Image
 
+
 # Find N newest files in a list of files!
-def Nmaxelements(list1, N):
+def n_max_elements(list1, n):
     final_list = []
 
-    for i in range(0, N):
+    for i in range(0, n):
         max1 = list1[0]
         for j in range(len(list1)):
             if os.path.getctime(list1[j]) > os.path.getctime(max1):
-                max1 = list1[j];
-        list1.remove(max1);
+                max1 = list1[j]
+        list1.remove(max1)
         final_list.append(max1)
 
     return final_list
+
 
 # Find the newest pics! (Those are the spotlight pics)
 def find_newest(path):
     files = os.listdir(path)
     paths = [os.path.join(path, basename) for basename in files]
-    return Nmaxelements(paths, 6)
+    return n_max_elements(paths, 6)
+
 
 # Copy pics to another folder!
 def copy():
-    files = find_newest(r'C:\Users\cguthrie\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets')
+    files = find_newest(r'C:\Users\cguthrie\AppData\Local\Packages\Microsoft.Windows.'
+                        r'ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets')
     for file in files:
         shutil.copy(file, r'C:\Users\cguthrie\Pictures\Background\test')
     print("Finished copying background pictures")
+
 
 # Delete the vertical pics cause they look bad with landscape monitors!
 def delete_vertical_pics():
@@ -42,11 +47,13 @@ def delete_vertical_pics():
             os.remove(path + '\\' + file)
             print("Removed " + file + '   X')
 
+
 # Make nicer looking file names!
 def create_file_name(i):
     now = datetime.datetime.now()
     path = r'C:\Users\cguthrie\Pictures\Background\test'
     return path + '\\' + now.strftime("%Y-%m-%d") + '_' + str(i) + '.jpg'
+
 
 # Rename the files so they are more clear!
 def rename():
@@ -61,6 +68,7 @@ def rename():
         os.rename(old_name, new_name)
         i += 1
     print("Renamed all files to date_i.jpg")
+
 
 # Finally, move the files into the bg folder!
 def move_to_bg():
@@ -77,4 +85,3 @@ def move_to_bg():
 
 # This does everything!! kinda
 move_to_bg()
-
